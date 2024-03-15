cec1e0fb42ab199ab3d770566d70835b100d0165
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-03-15 10:40:37 -0300

    Finalizando o código

--- orientada-objetos/src/entities/Product.java
+++ orientada-objetos/src/entities/Product.java
@@ -19,7 +19,7 @@ public class Product {
 	}
 	
 	public String toString() {
-		return name + ", $ " + price + ", " + quantity + " units, Total: $ " + totalValueInStock();
+		return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ " + String.format("%.2f", totalValueInStock());
 	}
 
 }
